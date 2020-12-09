9fc258ae84b611abb7931b63b1706e27eb1afb9d
Angel CHAPELET <mschap.xb@gmail.com>
2020-12-09 16:12:56 +0100

    page13

---
+++ src/Controller/AisShipTypeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/aisshiptype", name="aisshiptype_")
+ */
+class AisShipTypeController extends AbstractController
+{
+    /**
+     * @Route("/voirtous", name="voirtous"}
+     */
+    public function voirTous(): Response
+   {
+       $types = [
+           1 => 'Reserved',
+           2 => 'Wing In Ground',
+           3 => 'Special Category',
+           4 => 'High-Speed Craft',
+           5 => 'Special Category',
+           6 => 'Passenger',
+           7 => 'Cargo',
+           8 => 'Tanker',
+           9 => 'Other',
+       ];
+      return $this->render('aisshiptype/voirtous.html.twig', [
+                   'types' => $types,
+       ]);
+     
+   }
+}
+
---
+++ src/Entity/Navire.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NavireRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Regex;
+
+
+/**
+ * @ORM\Entity(repositoryClass=NavireRepository::class)
+ */
+class Navire
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=7)
+     * @Assert\Regex(
+     *     pattern="/[1-9]{7}/",
+     *     message="Le numéro IMO doit comporter 7 chiffres"
+     * )
+     */
+    private $imo;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\Length(
+     *                  min=3,
+     *                  max=100
+     *                  )
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=9)
+     */
+    private $mmsi;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $indicatifAppel;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $eta;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImo(): ?string
+    {
+        return $this->imo;
+    }
+
+    public function setImo(string $imo): self
+    {
+        $this->imo = $imo;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMmsi(): ?string
+    {
+        return $this->mmsi;
+    }
+
+    public function setMmsi(string $mmsi): self
+    {
+        $this->mmsi = $mmsi;
+
+        return $this;
+    }
+
+    public function getIndicatifAppel(): ?\DateTimeInterface
+    {
+        return $this->indicatifAppel;
+    }
+
+    public function setIndicatifAppel(?\DateTimeInterface $indicatifAppel): self
+    {
+        $this->indicatifAppel = $indicatifAppel;
+
+        return $this;
+    }
+
+    public function getEta(): ?\DateTimeInterface
+    {
+        return $this->eta;
+    }
+
+    public function setEta(?\DateTimeInterface $eta): self
+    {
+        $this->eta = $eta;
+
+        return $this;
+    }
+}
